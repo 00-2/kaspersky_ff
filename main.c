43b6ad547b100a06e082c25f54f15482ed1ced54
00-2 <9x02@mail.ru>
2022-10-13 20:11:45 +0300

    update comments

--- main.c
+++ main.c
@@ -49,13 +49,13 @@ int main(int argc, char** argv)
 {
     if (argc<2) {fprintf(stderr, "Wrong count of element. Required>=1 : Received: %d\n", argc-1); exit(-1);}
     const char *to_find = argv[1];
-    char *dir_to_find = malloc(sizeof(char)* MAXDIR);
-    char *to_regex = malloc(sizeof(char)*MAXDIR*7+1);
+    char *to_regex = malloc(sizeof(char)*MAXDIR*7+1); // because of replacement "*" with "[*\n ]*"
     strcpy(to_regex, to_find);
+    char *dir_to_find = malloc(sizeof(char)* MAXDIR);
     if (argc>=3) {
-        // check if end onto "/"
-        // check strlen == 0 in case /
-        if ((strlen(argv[2])-1)!=0 && argv[2][strlen(argv[2])-1] == '/'){ // without last /
+        // check if end with "/"
+        // check strlen != 0 in case "/"
+        if ((strlen(argv[2])-1)!=0 && argv[2][strlen(argv[2])-1] == '/'){ // ends with "/"
             strncpy(dir_to_find, argv[2], strlen(argv[2])-1);
         }
         else{ // strcpy
@@ -72,15 +72,14 @@ int main(int argc, char** argv)
             printf("filename_root is CWD : %s\n", dir_to_find);
     #endif
     }
-    // build regex for find special regex symbols, except ?*
+    // build regex for match special regex symbols, exclude "?*"
     regex_t regex_non_command_symbol;
     reti = regcomp(&regex_non_command_symbol, "[!@#$%^&(),.\":{}?|<>]", REG_EXTENDED);
     if (reti) {
         fprintf(stderr, "Could not compile regex\n");
         exit(1);
     }
-    // regexec require string buffer
-    char temp_buffer_regex_non_command_symbol[2] = "\0"; /* gives {\0, \0} */
+    char temp_buffer_regex_non_command_symbol[2] = "\0";    // regexec requires string buffer
     size_t to_find_size = strlen(to_find);
 
     int j = 0; // to_regex pos counter
