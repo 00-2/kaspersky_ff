6fe329a4360beec437c6315f4438b03ff95e7119
00-2 <9x02@mail.ru>
2022-10-13 15:14:24 +0300

    working regexp with ?,*

--- ff.sh
+++ ff.sh
@@ -3,12 +3,12 @@ if [[ $# -eq 0 ]] ; then
     echo "Wrong count of element. Required>=1 : Received: "$#
     exit 0
 fi
-to_find=$1
+to_find="$1"
 filename=$PWD
 if [[ $# -ge 2 ]] ; then
-    filename=$2
+    filename="$2"
 fi
-echo "$(sudo /usr/lib/ff/ff_bin $to_find $filename)"
+res="$(sudo /usr/lib/ff/ff_bin "$to_find" "$filename")"
 if ! [ "$res" = "" ]; then
   echo "$res"
 fi
