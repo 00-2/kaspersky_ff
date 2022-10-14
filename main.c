fb393ca2f4921a1957396c47d2df0deb61ebca67
00-2 <9x02@mail.ru>
2022-10-14 03:38:57 +0300

    rewrite logic, fix problems with pattern * / ?, with total size of path > 255.

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <dirent.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <stdlib.h>
 #include <regex.h>
-#define MAXDIR 255
+#include <sys/stat.h>
+#include <dirent.h>
+
 regex_t regex;
 int reti;
+long path_max;
+size_t _MAX_PATH_SIZE, size;
+char *buf;
+char *ptr;
 
+static inline void *mallocOrDie(size_t MemSize)
+{
+    void *AllocMem = malloc(MemSize);
+    /* Some implementations return null on a 0 length alloc,
+     * we may as well allow this as it increases compatibility
+     * with very few side effects */
+    if(!AllocMem && MemSize)
+    {
+        //TODO handle error
+        fprintf(stderr, "Cannot allocate memory\n");
+        exit(-1);
+    }
+    return AllocMem;
+}
 
 void printdir(char *dir, char* depth, const char *to_find)
 {
     DIR *dp;
     struct dirent *entry;
     struct stat statbuf;
+    size_t count_of_block_depth;
+    size_t count_of_block_entry;
     if ((dp=opendir(dir)) == NULL) {
         fprintf(stderr, "cannot open directory: %s\n", dir);
         return;
@@ -28,7 +49,9 @@ void printdir(char *dir, char* depth, const char *to_find)
                 strcmp("..", entry->d_name) == 0)
                 continue;
             /* recurse at a new indent level */
-            char *tempdepth = malloc(sizeof (char)*MAXDIR);
+            count_of_block_depth = strlen(depth) / _MAX_PATH_SIZE + 1;
+            count_of_block_entry = strlen(entry->d_name) / _MAX_PATH_SIZE + 1;
+            char *tempdepth = mallocOrDie((sizeof(char)*_MAX_PATH_SIZE) * (count_of_block_depth+count_of_block_entry) + 1);
             strcpy(tempdepth, depth);
             if (strcmp(tempdepth, "/") != 0)
                 strcat(tempdepth, "/");
@@ -45,48 +68,77 @@ void printdir(char *dir, char* depth, const char *to_find)
     closedir(dp);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
+    char *to_regex;
+    const char *to_find;
+    char *root_dir;
+    size_t count_of_block_depth_init;
+    size_t count_of_block_root_dir = 0;
+    path_max = pathconf(".", _PC_PATH_MAX);
+    if (path_max == -1)
+        _MAX_PATH_SIZE = 1024;
+    else if (path_max > 10240)
+        _MAX_PATH_SIZE = 10240;
+    else
+        _MAX_PATH_SIZE = path_max;
+
     if (argc<2) {fprintf(stderr, "Wrong count of element. Required>=1 : Received: %d\n", argc-1); exit(-1);}
-    const char *to_find = argv[1];
-    char *to_regex = malloc(sizeof(char)*MAXDIR*7+1); // because of replacement "*" with "[*\n ]*"
+    to_find = argv[1];
+    count_of_block_depth_init = strlen(to_find) / _MAX_PATH_SIZE + 1;
+    to_regex = calloc((_MAX_PATH_SIZE*7) * count_of_block_depth_init + 1, sizeof(char)); // because of replacement "*" with "[*\n ]*"
     strcpy(to_regex, to_find);
-    char *dir_to_find = malloc(sizeof(char)* MAXDIR);
+
     if (argc>=3) {
         // check if end with "/"
         // check strlen != 0 in case "/"
-        if ((strlen(argv[2])-1)!=0 && argv[2][strlen(argv[2])-1] == '/'){ // ends with "/"
-            strncpy(dir_to_find, argv[2], strlen(argv[2])-1);
+        count_of_block_root_dir = strlen(argv[2]) / _MAX_PATH_SIZE + 1;
+        root_dir = mallocOrDie((sizeof(char)*_MAX_PATH_SIZE*7) * count_of_block_root_dir + 1);
+        if ((strlen(argv[2])-1)!=0 && argv[2][strlen(argv[2])-1] == '/') { // ends with "/"
+            strncpy(root_dir, argv[2], strlen(argv[2])-1);
         }
-        else{ // strcpy
-            strcpy(dir_to_find, argv[2]);
+        else {
+            strcpy(root_dir, argv[2]);
         }
     #ifdef DEBUG
-            printf("FIND filename_root IN ARGV %s\n", dir_to_find);
+        printf("root_dir from ARGV %s\n", root_dir);
     #endif
     }
     else {
-        // Get the current working directory:
-        getcwd( dir_to_find, MAXDIR);
+        size = _MAX_PATH_SIZE;
+        for (root_dir = ptr = NULL; ptr == NULL; size *= 2) {
+            if ((root_dir = realloc(root_dir, size)) == NULL) {
+                //TODO handle error
+                fprintf(stderr, "Cannot reallocate memory\n");
+                exit(-1);
+            }
+
+            ptr = getcwd(root_dir, size);
+            if (ptr == NULL && errno != ERANGE) {
+                //TODO handle error
+                fprintf(stderr, "Error with getcwd\n");
+                exit(-1);
+            }
+        }
     #ifdef DEBUG
-            printf("filename_root is CWD : %s\n", dir_to_find);
+        printf("root_dir is CWD : %s\n", root_dir);
     #endif
     }
+
     // build regex for match special regex symbols, exclude "?*"
     regex_t regex_non_command_symbol;
-    reti = regcomp(&regex_non_command_symbol, "[!@#$%^&(),.\":{}|<>]", REG_EXTENDED);
+    reti = regcomp(&regex_non_command_symbol, "[!@#$%^&(),.\":{}|?<>]", REG_EXTENDED);
     if (reti) {
+        //TODO handle error
         fprintf(stderr, "Could not compile regex\n");
         exit(1);
     }
     char temp_buffer_regex_non_command_symbol[2] = "\0";    // regexec requires string buffer
-    size_t to_find_size = strlen(to_find);
-
-    int j = 0; // to_regex pos counter
-
-    for(int i = 0; i<to_find_size; ++i){
+    int i;
+    int j = 1; // to_regex counter
+    to_regex[0] = '^';
+    for(i = 0; i<strlen(to_find); ++i){
         temp_buffer_regex_non_command_symbol[0] = to_find[i];
-        if (to_find[i] == '?'){
+        if (to_find[i] == '?'){ // "?" -> "." because of POSIX
             to_regex[j]='.'; ++j;
             continue;
         }
@@ -110,9 +162,9 @@ int main(int argc, char** argv)
         }
     }
     strcat(to_regex, "$");
-#ifdef DEBUG
-    printf("to_regex value %s\n", to_regex);
-#endif
+    #ifdef DEBUG
+        printf("to_regex value %s\n", to_regex);
+    #endif
     // main regex pattern
     // validate in printdir
     reti = regcomp(&regex, to_regex, REG_EXTENDED);
@@ -120,9 +172,9 @@ int main(int argc, char** argv)
         fprintf(stderr, "Could not compile regex\n");
         exit(1);
     }
-    printdir(dir_to_find, dir_to_find, to_find);
-    free(dir_to_find);
+    printdir(root_dir, root_dir, to_find);
     free(to_regex);
+    free(root_dir);
     regfree(&regex);
     regfree(&regex_non_command_symbol);
     exit(0);
